664acdea5e1c95c0e2e967df7d4dda134c7273dd
lzamranoa <lzambranoa.software@gmail.com>
2022-07-19 15:55:33 -0500

    Creación del proyecto y rutas con lazy load

---
+++ src/app/paises/paises-routing.module.ts
@@ -0,0 +1,25 @@
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'selector',
+        component: SelectorPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'selector'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PaisesRoutingModule { }
